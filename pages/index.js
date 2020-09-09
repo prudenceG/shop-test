436cb4916e34285d1dbd363dc8824a7d705f1b83
Gaetan Boyron <gaetan.boyron@birchbox.com>
2020-09-09 17:39:38 +0200

    init commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,38 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import DefaultLayaout from '../components/DefaultLayout'
+import { withStyles, Button, Container, Grid, Typography } from '@material-ui/core'
+import Link from 'next/link'
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+const useStyles = theme => ({
+    container: {marginTop: theme.spacing(5)}
+});
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+const  Home = props => {
+    const {classes} = props
+    return (
+        <DefaultLayaout>
+            <Container maxWidth="sm" className={classes.container}>
+                <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+                    SuperShop
+                </Typography>
+                <Typography variant="h5" align="center" color="textSecondary" paragraph>
+                    Something short and leading about the collection below—its contents, the creator, etc.
+                    Make it short and sweet, but not too short so folks don&apos;t simply skip over it
+                    entirely.
+                </Typography>
+                <div className={classes.heroButtons}>
+                    <Grid container spacing={2} justify="center">
+                        <Grid item>
+                            <Link href="/boutique" passhref>
+                                <Button variant="contained" component="a">
+                                    La Boutique
+                                </Button>
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </div>
+            </Container>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+        </DefaultLayaout>
+    )
 }
+export default withStyles(useStyles)(Home)
