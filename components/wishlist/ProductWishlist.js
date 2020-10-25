756189eef93da841123edbd2dcefd6350f6c4c51
Prudence Gaboriau <prudence.gaboriau@ext.vallourec.com>
2020-10-25 19:38:32 +0100

    feat(master): adding text when there is no products in whislist

--- components/wishlist/ProductWishlist.js
+++ components/wishlist/ProductWishlist.js
@@ -1,9 +1,12 @@
-import { Grid } from '@material-ui/core';
+import { Grid, Typography } from '@material-ui/core';
 import WishedProductCard from './WishedProductCard';
 
 const ProductWishlist = ({wishlist}) => {
   return (
     <Grid container spacing={3} justify="center">
+      {!wishlist.length > 0 && <Typography variant="h6" component="p">
+        There isn't products in your wishlist
+      </Typography>}
       {wishlist.map(product => (
         <Grid key={product.id} item xs={12} sm={9}>
           <WishedProductCard product={product} />
