35aa7797541fad29f692b72d37e026ae78c50c7b
Prudence Gaboriau <prudence.gaboriau@ext.vallourec.com>
2020-11-05 18:35:02 +0100

    feat(master): adding code from material ui to avoid css bug on relaod page

--- pages/_app.js
+++ pages/_app.js
@@ -4,11 +4,21 @@ import { Cart } from './../store/cart';
 import { TransitionAlert } from '../store/transitionAlert';
 
 function MyApp({ Component, pageProps }) {
+
+  React.useEffect(() => {
+    // Remove the server-side injected CSS to display CSS on reload CTRL+R
+    const jssStyles = document.querySelector('#jss-server-side');
+
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
     <Cart.Provider>
       <Wishlist.Provider>
         <TransitionAlert.Provider>
-          <Component {...pageProps} />
+            <Component {...pageProps} />
         </TransitionAlert.Provider>
       </Wishlist.Provider>
     </Cart.Provider>
@@ -16,10 +26,10 @@ function MyApp({ Component, pageProps }) {
 }
 
 // It is possible to combine providers by creating a Store
-// Example of code available in ./state/index.js
+// Example of code available in ./store/index.js
 
 // _app.js
-// import { Store } from './../state/index';
+// import { Store } from './../store/index';
 
 // function MyApp({ Component, pageProps }) {
 //   return (
